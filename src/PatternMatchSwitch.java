79e09c1197ff0436ad062d1ec4c16f7261735746
amodkunwar <amodkunwar@gmail.com>
2023-04-09 18:13:37 +0530

    Pattern Switch

---
+++ src/PatternMatchSwitch.java
@@ -0,0 +1,20 @@
+
+public class PatternMatchSwitch {
+
+	/*
+	public static void whatType(Object o) {
+		switch (o) {
+		case String s -> System.out.println("String");
+		case Integer i -> System.out.println("Integer");
+		case null -> System.out.println("Null");
+		default -> System.out.println("None of these");
+		}
+	}
+	 */
+	
+	
+	public static void main(String[] args) {
+//		whatType("Amod");
+	}
+
+}
